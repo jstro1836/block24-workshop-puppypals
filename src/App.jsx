548195976be337d3ef7365dca8529e41f329d182
jstro1836 <39538412+jstro1836@users.noreply.github.com>
2024-04-30 22:18:55 -0500

    Completed workshop.

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,40 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    const [puppies, setPuppies] = useState(puppyList);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
     //console.log(puppies);
 
-  return (
-    <>
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-          })
-        }
-      </div>
-      
-    </>
-  )
-}
+    const handleClick = (id) => {
+      setFeatPupId(id);
+    }
+
+    const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+    //console.log(featuredPup);
+
+    return (
+      <>
+      <h1>Puppy Adoption Agency</h1>
+      <h2>Click a puppy to learn more about them!</h2>
+        <div className="App">
+          {puppies.map((puppy) => (
+            <div className="puppy-card" onClick={() => handleClick(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </div>
+          ))}
+        </div>
+  
+        {featPupId && featuredPup && (
+          <div className="featured-puppy">
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </>
+    );
+  }
 
 export default App
